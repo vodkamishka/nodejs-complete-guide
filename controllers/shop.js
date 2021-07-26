e34d98e902aea2eda613d47d781f923b1c448e09
Deynega Andrey <andrtomsk@narod.ru>
2021-07-26 12:05:47 +0700

    work with sequelize

--- controllers/shop.js
+++ controllers/shop.js
@@ -50,6 +50,7 @@ exports.getCart = (req, res, next) => {
 exports.postCart = (req, res, next) => {
     const prodId = req.body.productId;
     let fetchedCart;
+    let newQuantity = 1;
     req.user
         .getCart()
         .then(cart => {
@@ -61,15 +62,15 @@ exports.postCart = (req, res, next) => {
             if (products.length > 0) {
                 product = products[0];
             };
-            let newQuantity = 1;
             if (product) {
-
+                const oldQuantity = product.cartItem.quantity;
+                newQuantity = oldQuantity + 1;
+                return product;
             }
             return Product.findByPk(prodId)
-                .then(product => {
-                    return fetchedCart.addProduct(product, { through : { quantity: newQuantity }})
-                })
-                .catch(err => console.log(err))
+        })
+        .then(product => {
+            return fetchedCart.addProduct(product, { through : { quantity: newQuantity }})
         })
         .then(() => res.redirect('/cart'))
         .catch(err => console.log(cart))
@@ -77,11 +78,19 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    console.log('prodId', prodId);
-    Product.findById(prodId, product => {
-        Cart.deleteProduct(prodId, product.price);
-        res.redirect('/cart');
-    })
+    req.user
+        .getCart()
+        .then(cart => {
+            return cart.getProducts({where: { id: prodId } })
+        })
+        .then(products => {
+            const product = products[0];
+            product.cartItem.destroy();
+        })
+        .then(() => {
+            res.redirect('/cart');
+        })
+        .catch(err => console.log(err))
 }
 
 exports.getOrders = (req, res, next) => {
