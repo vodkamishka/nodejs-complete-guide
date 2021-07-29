2ce8d922aa5c3359e8b3588f47932a037ab275db
Deynega Andrey <andrtomsk@narod.ru>
2021-07-29 22:48:01 +0700

    finished sessions

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,6 +13,7 @@ exports.postLogin = (req, res, next) => {
         .then(user => {
             req.session.isLoggedIn = true
             req.session.user = user;
+            req.session.save()
             res.redirect('/');
         })
         .catch(err => console.log(err));
